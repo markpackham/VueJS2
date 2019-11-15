823f01598171de9167a5c8705f0cdc0a2fb5143a
Mark Packham <markpackham1@gmail.com>
2019-11-15 19:47:16 +0000

    learning about nested components https://youtu.be/ko5Eijiaozo?t=84

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<h1>{{title}}</h1>
+<p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Your first Vue file"
+    };
+  },
+  methods: {
+    greeting: function() {
+      return "Howdy cowboy";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
