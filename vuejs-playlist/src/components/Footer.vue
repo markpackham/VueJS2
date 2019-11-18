cc971b6a83eb20b84f52d0b51649d7ea26514394
Mark Packham <markpackham1@gmail.com>
2019-11-18 18:26:30 +0000

    moving onto primative vs reference types, https://youtu.be/gfT0udzYkyw?t=13

---
+++ vuejs-playlist/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<footer>
+    <p>{{ copyright }}</p>
+</footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: "Copyright 2019"
+    };
+  }
+};
+</script>
+
+<style scoped>
+footer {
+  background: #222;
+  padding: 6px;
+}
+p {
+  color: red;
+  text-align: center;
+}
+</style>
